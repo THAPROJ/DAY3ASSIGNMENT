65471bb45f47abb71c9bb2e4a8ab14d866dbf21c
THAPROJ <93243527+THAPROJ@users.noreply.github.com>
2021-11-20 22:01:57 +0530

    Update program.cs

--- Program.cs
+++ Program.cs
@@ -18,9 +18,7 @@ namespace LINECOMPARISION
             double y1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter any  value y2: ");
             double y2 = Convert.ToDouble(Console.ReadLine());
-
-
-            double distance = Convert.ToDouble(Console.Read());
+            
 
             distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
             Console.WriteLine(distance);
